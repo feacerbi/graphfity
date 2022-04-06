b666e064c648a09ceb6062377d409380982127f4
Iván Carrasco <ivan.carrasco.dev@gmail.com>
2022-04-06 23:59:30 +0200

    add changes in GraphVisitor in order to use not external libraries, only kotlin basic classes

--- src/main/kotlin/com/github/ivancarras/graphfity/plugin/domain/model/GraphVisitor.kt
+++ src/main/kotlin/com/github/ivancarras/graphfity/plugin/domain/model/GraphVisitor.kt
@@ -1,23 +1,22 @@
 package com.github.ivancarras.graphfity.plugin.domain.model
 
 import com.github.ivancarras.graphfity.plugin.presentation.util.notContains
-import sun.misc.Queue
+import java.util.*
 import kotlin.collections.ArrayList
 
 class GraphVisitor<T>(private val graph: Graph<T>) {
     fun breadthFirstSearch(source: Vertex<T>): ArrayList<Vertex<T>> {
-        val queued = Queue<Vertex<T>>()
+        val queued: Queue<Vertex<T>> = LinkedList()
         val visited = arrayListOf<Vertex<T>>()
-
-        queued.enqueue(source)
+        queued.add(source)
         visited.add(source)
 
-        while (queued.isEmpty.not()){
-            val vertex = queued.dequeue()
+        while (queued.isNotEmpty()){
+            val vertex = queued.poll()
             val neighborsEdges = graph.edges(vertex)
             neighborsEdges.forEach {
                 if(visited.notContains(it.destination)){
-                    queued.enqueue(it.destination)
+                    queued.add(it.destination)
                 }
             }
         }
